17a106042f87de17a5c5870ec4d8001f38a49021
1 <40x146@gmail.com>
2025-06-07 19:41:53 +0300

    full fix& work btn/nowork func\power

--- src/components/bufs.vue
+++ src/components/bufs.vue
@@ -92,36 +92,51 @@ function clickBonus(){
   <div v-if="showLine" class="first-line"></div>
   <div v-if="showLines" class="top-lines"></div>
   <button
-      :disabled="bonusActiveCount||bonusCooldownCount"
+      :disabled="bonusActiveCount || bonusCooldownCount"
       class="bonus1"
-      @click="clickBonus">BONUS</button>
+      @click="clickBonus"
+  >
+    BONUS
+  </button>
   <div v-if="bonusCooldownCount" class="bonus-cooldown-bar"></div>
-
   <div class="button-container">
     <div class="btn-with-line">
       <div class="status-text" v-if="isCooldownCount || isActiveCount">
-        {{ isActiveCount ? 'Active...': 'Кулдаун...' }}
+        {{ isActiveCount ? 'Active...' : 'Кулдаун...' }}
       </div>
-
-      <button :disabled="isActiveCount || isCooldownCount" class="power" @click="clickHandler">
+      <button
+          :disabled="isActiveCount || isCooldownCount"
+          class="power"
+          @click="clickHandler"
+      >
         <img src="@/assets/boost.png" alt="Boost Power" class="icon" />
       </button>
-
       <div v-if="showLine" class="line"></div>
     </div>
     <div class="btn-with-line2">
-      <div class="status-text2" v-if="isCooldownPower||isActivePower">
-          {{ isActivePower? 'Active...': 'Кулдаун...'}}
+      <div class="status-text2" v-if="isCooldownPower || isActivePower">
+        {{ isActivePower ? 'Active...' : 'Кулдаун...' }}
       </div>
-      <button :disabled="isActivePower||isCooldownPower" class="power-of-num" @click="clickHaandler">
-         <img src="@/assets/boostspeed.png" alt="Boost Count" class="icon"/>
-      </button>
+      <div class="button-wrapper">
+        <button
+            :disabled="isActivePower || isCooldownPower"
+            class="power-of-num"
+            @click="clickHaandler"
+        >
+          <img src="@/assets/boostspeed.png" alt="Boost Count" class="icon" />
+        </button>
+        <div v-if="showLines" class="lines"></div>
+      </div>
     </div>
   </div>
 </template>
 
 
 <style scoped>
+.button-wrapper {
+  position: relative;
+  display: inline-block;
+}
 .btn-with-line {
   display: flex;
   flex-direction: column;
@@ -165,7 +180,25 @@ function clickBonus(){
     width: 0;
   }
 }
-
+.line {
+  width: 60px;
+  height: 4px;
+  background-color: red;
+  margin-top: 6px;
+  border-radius: 2px;
+  animation: pulseLine 1s infinite ease-in-out;
+  margin-left:50px;
+}
+@keyframes pulseLine {
+   0%, 100% {
+     opacity: 0.3;
+     transform: translateX(-50%) scaleX(1);
+   }
+   50% {
+     opacity: 1;
+     transform: translateX(-50%) scaleX(1.1);
+   }
+ }
 .top-lines {
   position: fixed;
   top: 6px;
@@ -178,6 +211,31 @@ function clickBonus(){
   border-radius: 0 4px 4px 0;
   box-shadow: 0 0 8px rgba(0, 0, 255, 0.4);
 
+}
+
+.lines {
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 60px;
+  height: 4px;
+  background-color: #00f;
+  border-radius: 2px;
+  margin-top: 4px;
+  animation: pulseLine 1s infinite ease-in-out;
+  box-shadow: 0 0 6px rgba(0, 0, 255, 0.5);
+}
+
+@keyframes pulseLine {
+  0%, 100% {
+    opacity: 0.3;
+    transform: translateX(-50%) scaleX(1);
+  }
+  50% {
+    opacity: 1;
+    transform: translateX(-50%) scaleX(1.1);
+  }
 }
 
 .button-container {
@@ -224,13 +282,7 @@ function clickBonus(){
   transform: scale(1.2);
 }
 
-.line {
-  width: 60px;
-  height: 4px;
-  background-color: red;
-  margin-top: 6px;
-  border-radius: 2px;
-}
+
 .bonus1 {
   position: fixed;
   top: 50%;
